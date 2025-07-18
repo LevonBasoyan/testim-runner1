b59823ebcf74c8d726369159d6464583d1fab611
git.ttsoft <git.ttsoft@gmail.com>
2025-07-18 17:02:05 +0400

    fixed serverjs

--- server.js
+++ server.js
@@ -15,27 +15,19 @@ app.post("/run-testim", (req, res) => {
 
   res.send({ message: "Test started" });
 
-npx testim \
-  --token "qhRgbWWMuLAVcKAmfH93UMt8p2elCyiyKBSGxSf83VG57SdtoP" \
-  --project "usw2RRRRFhuk6SLPxTmpc221" \
-  --testId "jI3lPRlYjOsNo4sl" \
-  --grid "Testim-Grid" \
-  --use-local-chrome-driver
-
-
+  const command = `npx testim --token "qhRgbWWMuLAVcKAmfH93UMt8p2elCyiyKBSGxSf83VG57SdtoP" --project "usw2RRRRFhuk6SLPxTmpc221" --testId "jI3lPRlYjOsNo4sl" --grid "Testim-Grid" --use-local-chrome-driver`;
 
   exec(command, (err, stdout, stderr) => {
-  if (err) {
-    console.error("❌ ERROR:", err);
-  }
-  if (stdout) {
-    console.log("✅ STDOUT:", stdout);
-  }
-  if (stderr) {
-    console.error("⚠️ STDERR:", stderr);
-  }
-});
-
+    if (err) {
+      console.error("❌ ERROR:", err);
+    }
+    if (stdout) {
+      console.log("✅ STDOUT:", stdout);
+    }
+    if (stderr) {
+      console.error("⚠️ STDERR:", stderr);
+    }
+  });
 });
 
 app.listen(3000, () => {
