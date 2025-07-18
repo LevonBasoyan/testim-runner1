8bcfcbbb94bf2c7bbb234bc5ab6b4b7148685778
git.ttsoft <git.ttsoft@gmail.com>
2025-07-18 10:46:12 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { exec } = require("child_process");
+
+const app = express();
+app.use(express.json());
+
+app.post("/run-testim", (req, res) => {
+  res.send({ message: "Test started" });
+
+  const command = `npx testim --token "qhRgbWWMuLAVcKAmfH93UMt8p2elCyiyKBSGxSf83VG57SdtoP" --project "usw2RRRRFhuk6SLPxTmpc221" --testId "96EJRqkb6s9GlHNH" --grid "Testim-Grid"`;
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) console.error("❌ ERROR:", err);
+    else console.log("✅ TESTIM OUTPUT:\n", stdout || stderr);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("🚀 Server ready");
+});
